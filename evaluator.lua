848381230d1d9853d1b244bec9cbaf861107d6f9
Glenn G. Chappell <ggchappell@alaska.edu>
2024-04-02 11:10:51 -0800

    evaluator.lua: FINISH - write func eval

--- evaluator.lua
+++ evaluator.lua
@@ -1,6 +1,7 @@
--- evaluator.lua  UNFINISHED
+-- evaluator.lua
 -- Glenn G. Chappell
--- 2024-03-31
+-- Started: 2024-03-31
+-- Updated: 2024-04-01
 --
 -- For CS 331 Spring 2024
 -- Evaluator for Arithmetic Expression AST (rdparser3.lua format)
@@ -25,8 +26,47 @@ local SIMPLE_VAR = 3
 --
 -- Example of a simple tree-walk interpreter.
 function evaluator.eval(ast, vars)
-    -- TODO: WRITE THIS!!!
-    return 42  -- DUMMY
+    local result, varname, op, arg1, arg2
+
+    assert(type(ast) == "table")
+    assert(type(vars) == "table")
+    assert(#ast >= 1)
+    if ast[1] == NUMLIT_VAL then         -- Numeric literal
+        assert(#ast == 2)
+        assert(type(ast[2]) == "string")
+        result = tonumber(ast[2])
+    elseif ast[1] == SIMPLE_VAR then     -- Named variable
+        assert(#ast == 2)
+        varname = ast[2]
+        assert(type(varname) == "string")
+        result = vars[varname]
+        if result == nil then      -- Undefined variable
+            result = 0
+        end
+    elseif type(ast[1] == "table") then  -- Operator
+        assert(#ast == 3)
+        assert(#ast[1] == 2)
+        assert(ast[1][1] == BIN_OP)
+        op = ast[1][2]
+        assert(type(op) == "string")
+        arg1 = evaluator.eval(ast[2], vars)
+        arg2 = evaluator.eval(ast[3], vars)
+        if op == "+" then
+            result = arg1 + arg2
+        elseif op == "-" then
+            result = arg1 - arg2
+        elseif op == "*" then
+            result = arg1 * arg2
+        else
+            assert(op == "/")
+            result = arg1 / arg2   -- Let IEEE floating-point handle
+                                   --  things like div by zero
+        end
+    else
+        assert(false)                    -- BAD AST
+    end
+
+    return result
 end
 
 
